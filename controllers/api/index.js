786e796520e62ef5e7c7bf19f73c0576cfc97002
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-07-11 18:56:30 -0700

    added files for api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,11 @@
 //routes 
 
 const router = require('express').Router();
+// const userRoutes = require('./user-routes');
+// const postRoutes = require('./post-routes');
+// const commentRoutes = require('./comment-routes')
 
+// router.use('/t', userRoutes);
+// router.use('/post-routes', postRoutes);
+// router.use('/comments-routes', commentRoutes);
 module.exports = router;
