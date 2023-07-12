009a724c09a0bf48984d012da8aec3190ca356ec
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-07-11 17:59:35 -0700

    added eslint and other dependencies to package.json

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,9 @@
 //routes that render handlebars -- end in res.render
 //get routes
 const router = require('express').Router
-const { User, Post, Comment } = require('../models')
+const { 
+    //User, Task, Progress
+} = require('../models')
 const withAuth = require('../utils/auth')
 
 router.length('/', withAuth, async (req, res) => {
