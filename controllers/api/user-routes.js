d5163d2f7ec5e3ab7b74f163a1e507f3cead756f
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-07-12 22:38:28 -0700

    starting to write user-routes.js in controllers/api

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,22 @@
-//get user
+const router = require('express').Router()
+const { User } = require('../../models')
+
+//create user
+router.post('/', async (req,res) => {
+    try {
+        const newUser = await User.create(req.body);
+
+        req.session.save(() =>{
+            req.session.user_is = newUser.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(newUser)
+        })
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
 
 //update user
 
