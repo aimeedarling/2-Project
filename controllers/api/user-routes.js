ae8d09e4383c9e33efab878ec1ef541989b3657b
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-07-13 17:21:47 -0700

    updated user route, added info to readme

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -18,7 +18,44 @@ router.post('/', async (req,res) => {
     }
 })
 
-//update user
+//login
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: {email: req.body.email}})
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again'})
+            return
+        }
+        const validPassword = await userData.checkPassword(req.body.password)
 
-//destroy user
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again'})
+            return
+        }
+        
+        req.sessionID.save(() => {
+            req.session.user_is = userData.id
+            req.session.logged_in = true
 
+            res.json({ user: userData, message: 'Success, you are now logged in!'})
+        })
+    } catch (error) {
+        res.status(400).json(error)
+        
+    }
+})
+
+
+//logout user
+
+router.post('./logout', (req,res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end()
+        })
+    } else {
+        res.status(404).end()
+    }
+})
+
+module.exports = router;
