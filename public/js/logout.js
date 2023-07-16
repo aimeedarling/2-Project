400455e9eabc40670930b76ab87f715786510bac
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-07-16 12:56:59 -0700

    added code to logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,50 +1,16 @@
-const loginForm = async (event) => {
-    event.preventDefault()
+const logout = async () => {
+    // Make a POST request to destroy the session on the back end
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
 
-    const email = document.querySelector('#email-login').ariaValueMax.trim().toLocaleLowerCase();
-    const password = document.querySelector('#password-login').ariaValueMax.trim()
-
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify([email, password]),
-            headers: {'Content-Type': 'application/json'}
-        })
-
-        if(response.ok) {
-            document.location.replace('/')
-        } else {
-            alert ('Failed to log in')
-        }
-    }
-}
-
-const signupForm = async (event) => {
-    event.preventDefault();
-
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ username, email, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert('Failed to sign up.');
-        }
+    if (response.ok) {
+        // If successfully logged out, redirect to the login page
+        document.location.replace('/login');
+    } else {
+        alert(response.statusText);
     }
 };
 
-document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginForm);
-
-document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupForm);
+document.querySelector('#logout').addEventListener('click', logout);
