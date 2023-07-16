0f5023bf77d607c6ce1268c445ee1d049b125bf1
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-07-16 13:14:56 -0700

    changes from Kevin

--- controllers/api/task-routes.js
+++ controllers/api/task-routes.js
@@ -1,30 +1,67 @@
-//get task
-router.get('/', async (req,res) => {
-    //get all tasks
-    try {
-        const tasks = await Task.findAll({
-            include: [{model: Task}]
-        })
-        res.status(200).json(tasks)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+const router = require('express').Router();
+const { Task } = require('../../models');
 
-//create new task
+/*
+ * This route creates a new task.
+ */
+router.post('/', async (req, res) => {
+    try {
+        const newTask = await Task.create(req.body);
+        res.status(200).json(newTask);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+/*
+ * This route gets all tasks.
+ */
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+});
+
+/*
+ * This route gets a specific task by id.
+ */
 router.get('/:id', async (req, res) => {
-    //get one task by it's id 
-    try {
-        const singleTask = await Task.findbyPk(req.params.id, {
-            include: [Task]
-        })
-        res.status(200).json(allCategories)
-    } catch (error) {
-        res.sendStatus(500).json(error)
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+        res.status(404).end();
+    } else {
+        res.status(200).json(task);
     }
-})
+});
 
-//update task
+/*
+ * This route updates a specific task by id.
+ */
+router.put('/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            res.status(404).end();
+        } else {
+            task.update(req.body);
+            await task.save();
+            res.status(200).json(task);
+        }
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
 
+/*
+ * This route deletes a specific task by id.
+ */
+router.delete('/:id', async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+        res.status(404).end();
+    } else {
+        await task.remove();
+        res.status(200).end();
+    }
+});
 
-//delete task
+module.exports = router;
