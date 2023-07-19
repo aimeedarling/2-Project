48998530c1beb82331a87a091a8c1a9ead27cdad
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-07-18 18:50:54 -0700

    working create task event listner, no more html error on / route

--- public/js/create-task.js
+++ public/js/create-task.js
@@ -4,15 +4,17 @@ const createFormHandler = async (event) => {
     //collects value from the create form
     const newTask = document.querySelector("#task").value.trim();
 
-
+    const response = await fetch('/api/task-routes', {
+        method: 'POST',
+        body: JSON.stringify({ task_name: newTask }),
+        headers: { 'Content-Type': 'application/json' }
+    });
     if (response.ok) {
-        const response = await fetch('api/task-routes', {
-            method: 'POST',
-            body: JSON.stringify({ newTask }), // Use { newTask } instead of {newTask}
-            headers: { 'Content-Type': 'application/json' }
-        });
+        console.log(response)
     } else {
         console.error('Error creating the task:', response.statusText);
     }
 
 }
+
+document.querySelector('#create').addEventListener('click', createFormHandler)
