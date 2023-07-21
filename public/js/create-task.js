c11c678ac3c61bc1817b448780a537a36696427a
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-07-20 22:59:21 -0400

    attach user to task

--- public/js/create-task.js
+++ public/js/create-task.js
@@ -4,9 +4,19 @@ const createFormHandler = async (event) => {
     //collects value from the create form
     const newTask = document.querySelector("#task").value.trim();
 
+    //need to pull this from session
+    const user_id = 1;
+
+    //Pass progress, task_name, user_id in this fetch call
+    //get user_id from session
+
     const response = await fetch('/api/task-routes', {
         method: 'POST',
-        body: JSON.stringify({ task_name: newTask }),
+        body: JSON.stringify({ 
+            task_name: newTask,
+            progress: 'todo',
+            user_id: user_id
+        }),
         headers: { 'Content-Type': 'application/json' }
     });
     if (response.ok) {
