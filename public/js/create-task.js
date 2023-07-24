9674d34a600594b62d82208cba16fd88e7dfafc9
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-07-23 21:04:24 -0700

    added reload after task is created

--- public/js/create-task.js
+++ public/js/create-task.js
@@ -1,26 +1,29 @@
 const createFormHandler = async (event) => {
     event.preventDefault();
+    console.log('click')
 
     //collects value from the create form
     const newTask = document.querySelector("#task").value.trim();
+    console.log(newTask)
 
     //need to pull this from session
-    const user_id = req.session.user_id;
+    const userId = 1
+
 
     //Pass progress, task_name, user_id in this fetch call
     //get user_id from session
 
-    const response = await fetch('/api/task', {
+    const response = await fetch('/api/tasks', {
         method: 'POST',
         body: JSON.stringify({ 
             task_name: newTask,
             progress: 'todo',
-            user_id: user_id
+            user_id: userId
         }),
         headers: { 'Content-Type': 'application/json' }
     });
     if (response.ok) {
-        console.log(response)
+        document.location.reload()
     } else {
         console.error('Error creating the task:', response.statusText);
     }
